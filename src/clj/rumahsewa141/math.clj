a85c11d0b31a015c82509a13ca6fd94058b03abd
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-05-08 16:57:58 +0800

    Add missing comments.

--- src/clj/rumahsewa141/math.clj
+++ src/clj/rumahsewa141/math.clj
@@ -10,7 +10,8 @@
     :else n))
 
 (defn parse-double
-  "Check if num string match decimal regex with a precision of 2."
+  "Check if num string matches decimal regex with a precision of 2.
+  If match, parse to double, else return 0.00."
   [num]
   (if (re-find #"^[0-9]+(\.[0-9]{1,2})?$" num)
     (Double/parseDouble num)
