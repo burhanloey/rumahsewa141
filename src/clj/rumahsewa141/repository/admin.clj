10028e60ac151a2d5f859b7f1ecfb5de9b23bf03
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-05-08 16:42:04 +0800

    Add comments for services (or repository)

--- src/clj/rumahsewa141/repository/admin.clj
+++ src/clj/rumahsewa141/repository/admin.clj
@@ -1,11 +1,16 @@
 (ns rumahsewa141.repository.admin
   (:require [rumahsewa141.db.core :as db]))
 
-(defn display-info [{:keys [username nickname phone_no]}]
+(defn display-info
+  "Display user info in hashmap. The value for :name is the nicname
+  if it exists. If not, just use the username."
+  [{:keys [username nickname phone_no]}]
   {:name (if (clojure.string/blank? nickname)
            username
            nickname)
    :contact_no phone_no})
 
-(defn display-all-admins-info []
+(defn display-all-admins-info
+  "Apply display-info function for all administrators."
+  []
   (map display-info (db/fetch-all-admins)))
--- src/clj/rumahsewa141/repository/transaction.clj
+++ src/clj/rumahsewa141/repository/transaction.clj
@@ -3,27 +3,41 @@
             [rumahsewa141.util :refer [do-to-selected
                                        parse-int]]))
 
-(defn add-transaction-by-user [sign rent internet others]
+(defn add-transaction-by-user
+  "Return a function that will create transaction given the bill
+  values. The sign determines the type of transaction,
+  + for billed,
+  - for paid."
+  [sign rent internet others]
   (comp
    (partial db/create-transaction!)
    (partial zipmap [:rent :internet :others :user_id])
    (partial conj [(sign rent) (sign internet) (sign others)])
    (partial parse-int)))
 
-(defn add-transactions-by-users [users sign rent internet others]
+(defn add-transactions-by-users
+  "Apply add-transaction-by-user to all selected users."
+  [users sign rent internet others]
   (do-to-selected users (add-transaction-by-user sign rent internet others)))
 
 (defn transactions-count
+  "Fetch all transactions count for all users. If identity supplied,
+  return a function that fetch all transactions count for that user."
   ([]
    (db/fetch-all-transactions-count))
   ([{{id :id} :identity}]
    (partial db/fetch-transactions-count-by-user {:user_id id})))
 
 (defn latest-transactions
+  "Return a function that will fetch latest transactions for all
+  users if requested by admin, else, fetch latest transactions for
+  that user."
   [{{id :id admin :admin} :identity}]
   (if (true? admin)
     (partial db/fetch-all-latest-transactions)
     (comp (partial db/fetch-latest-transactions-by-user) (partial merge {:user_id id}))))
 
-(defn user-bills [{{id :id} :identity}]
+(defn user-bills
+  "Return a function that will fetch total bills for the user."
+  [{{id :id} :identity}]
   (partial db/fetch-total-bills-by-user {:user_id id}))
