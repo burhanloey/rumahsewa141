e62f3d5ab26a50853833187c9546a7eca8b203ac
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-05-08 17:43:32 +0800

    Add comments for routes

--- src/clj/rumahsewa141/routes/admin.clj
+++ src/clj/rumahsewa141/routes/admin.clj
@@ -16,22 +16,34 @@
             [rumahsewa141.views :refer [history-view]]
             [rumahsewa141.math :refer [parse-double]]))
 
-(defn update-registration [{{action :action} :params}]
+(defn update-registration
+  "Update registration controller. Redirect to registration page
+  afterwards."
+  [{{action :action} :params}]
   (when-let [_ (update-registration-config action)]
     (redirect "/admin/settings/registration")))
 
-(defn- add-transaction [users sign rent internet others]
+(defn- add-transaction
+  "Add transaction for selected users. Render success page when
+  success."
+  [users sign rent internet others]
   (when-let [_ (add-transactions-by-users users sign rent internet others)]
     (layout/render "success.html" {:title "Done!"
                                    :description (if (pos? (sign 1))
                                                   "Selected users billed successfully."
                                                   "Payment received.")})))
 
-(defn do-transaction [sign
-                      {{users           :users
-                        rent-string     :rent
-                        internet-string :internet
-                        others-string   :others  } :params}]
+(defn do-transaction
+  "Do transaction controller.
+
+  If no user selected, render error page asking to select user.
+  If bills requested is zero, render page saying it is no use.
+  Else, do the transactions."
+  [sign
+   {{users           :users
+     rent-string     :rent
+     internet-string :internet
+     others-string   :others  } :params}]
   (let [rent     (parse-double rent-string)
         internet (parse-double internet-string)
         others   (parse-double others-string)]
@@ -40,19 +52,30 @@
       (zero? (+ rent internet others)) (layout/render "error_message.html" {:description "No point if no money involved."})
       :else (add-transaction users sign rent internet others))))
 
-(defn- manage-users [users action]
+(defn- manage-users
+  "Manage selected users according to action. Redirect to management
+  page afterwards."
+  [users action]
   (when-let [_ (update-users users action)]
     (redirect "/admin/manage")))
 
-(defn do-manage [{{:keys [users action]} :params}]
+(defn do-manage
+  "Management controller.
+
+  If no user selected, render page asking to select user.
+  Else, manage selected users."
+  [{{:keys [users action]} :params}]
   (if (nil? users)
     (layout/render "error_message.html" {:description "Please select a user."})
     (manage-users users action)))
 
-(defn admin-page [section
-                  get-content-fn
-                  {{username :username} :identity}
-                  & [subsection]]
+(defn admin-page
+  "Render admin page according to supplied section. The get-content-fn
+  will supply the content for the section. The identity will determine
+  whether to render page for admin or normal user. The subsection is
+  optional since some of the page have more section, for example in
+  page 'settings'."
+  [section get-content-fn {{username :username} :identity} & [subsection]]
   (layout/render "member.html" (merge {:username username
                                        :admin true
                                        :section section
@@ -61,7 +84,9 @@
                                         nil
                                         (get-content-fn)))))
 
-(defn settings-page [subsection req & [get-content-fn]]
+(defn settings-page
+  "Render settings page."
+  [subsection req & [get-content-fn]]
   (admin-page "settings" get-content-fn req subsection))
 
 (defroutes admin-routes
--- src/clj/rumahsewa141/routes/register.clj
+++ src/clj/rumahsewa141/routes/register.clj
@@ -11,29 +11,37 @@
             [rumahsewa141.repository.config :refer [registration-allowed?]]
             [rumahsewa141.repository.user :refer [create-user]]))
 
-(defn- register-user! [username password nickname phone_no]
+(defn- register-user!
+  "Register user with the supplied informations. Render success page
+  afterwards."
+  [username password nickname phone_no]
   (when-let [_ (create-user username password nickname phone_no)]
     (layout/render "success.html" {:title "Success!"
                                    :description "You have been registered."})))
 
-(defn- valid-registration? [params]
+(defn- valid-registration?
+  "Validate registration details. Other than required values, the
+  function checks whether username already existed."
+  [params]
   (b/valid? params
             :username [v/required available-username]
             :password v/required
             :confirm v/required))
 
-(defn- get-registration-error [params]
+(defn- get-registration-error
+  "Get registration error message from validation."
+  [params]
   (apply str (:username (first (b/validate
                                 params
                                 :username [v/required available-username]
                                 :password v/required
                                 :confirm v/required)))))
 
-(defn do-registration [{{:keys [username
-                                password
-                                confirm
-                                nickname
-                                phone_no] :as params} :params}]
+(defn do-registration
+  "Registration controller. Register user when the registration
+  details are valid, and the password is confirmed. Render error
+  message according to what is missing."
+  [{{:keys [username password confirm nickname phone_no] :as params} :params}]
   (if (valid-registration? params)
     (if (= password confirm)
       (register-user! username password nickname phone_no)
